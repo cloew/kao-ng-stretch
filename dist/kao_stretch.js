63c6d5ec71904127ed59809cc0cc169d209bb59d
Chris Loew <cloew123@gmail.com>
2015-09-29 16:30:41 -0600

    Added basic Kao Stretch file

---
+++ dist/kao_stretch.js
@@ -0,0 +1,27 @@
+$traceurRuntime.ModuleStore.getAnonymousModule(function() {
+  "use strict";
+  angular.module("kao.stretch", []).directive("stretchToBottom", function($window, $document, $timeout) {
+    return {
+      restrict: "A",
+      replace: true,
+      link: function(scope, element, attrs) {
+        var originalHeight = void 0;
+        var applyHeight = function() {
+          var newHeight = $window.innerHeight - element.offset().top;
+          element.height(newHeight);
+        };
+        angular.element($window).bind("resize", function() {
+          scope.$apply(function() {
+            applyHeight();
+          });
+        });
+        $document.ready(function() {
+          $timeout(applyHeight, 200);
+        });
+      }
+    };
+  });
+  return {};
+});
+
+//# sourceMappingURL=kao_stretch.map
